e0a35637f5caccdfaaa730a745736f3416004f70
Marko990330 <marko990330@naver.com>
2024-05-02 18:21:42 +0900

    주석추가 및 한글화 완료

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-# pip install googletrans==3.1.0a0
+# pip install googletrans 사용시 오류 -> pip install googletrans==3.1.0a0 사용
+
 
 from googletrans import Translator
 import random
@@ -8,7 +9,6 @@ def translate_to_korean(text):
     translation = translator.translate(text, dest='ko')
     return translation.text
 
-# This list contains the random responses (you can add your own or translate them into your own language too)
 random_responses = ["That is quite interesting, please tell me more.",
                     "I see. Do go on.",
                     "Why do you say that?",
@@ -16,7 +16,7 @@ random_responses = ["That is quite interesting, please tell me more.",
                     "Let's change the subject.",
                     "Did you catch the game last night?"]
 
-first_message = """Hello, I am Marvin, the simple robot.
+first_message = """Hello, I am Marvin, the simple robot. 
 You can end this conversation at any time by typing '잘가'
 After typing each answer, press 'enter'
 How are you today?"""
@@ -26,10 +26,9 @@ print(first_message)
 
 
 while True:
-    # wait for the user to enter some text
+    # 사용자입력 대기
     user_input = input("> ")
     if user_input== "잘가":
-        # if they typed in 'bye' (or even BYE, ByE, byE etc.), break out of the loop
         break
     else:
         response = translate_to_korean(random.choices(random_responses)[0])
